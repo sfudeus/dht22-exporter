fc711f1f6e4143619f64bb116499aeaf89cb9f7f
Stephan Fudeus <stephan.fudeus@1und1.de>
2019-11-29 22:52:59 +0100

    cleanup

--- main.go
+++ main.go
@@ -2,25 +2,22 @@ package main // import "github.com/sfudeus/dht22-exporter"
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/MichaelS11/go-dht"
 	"github.com/jessevdk/go-flags"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/MichaelS11/go-dht"
 )
-
-var port io.ReadWriteCloser
 
 var options struct {
 	Port      int64  `long:"port" default:"8080" description:"The address to listen on for HTTP requests." env:"EXPORTER_PORT"`
 	Interval  int64  `long:"interval" default:"60" env:"INTERVAL" description:"The frequency in seconds in which to gather data"`
-	Pin		    int64 `long:"pin" default:"4" description:"The GPIO pin to use"`
+	Pin       int64  `long:"pin" default:"4" description:"The GPIO pin to use"`
 	MeterName string `long:"metername" description:"The name of your meter, to uniquely name them if you have multiple"`
 	Debug     bool   `long:"debug" description:"Activate debug mode"`
 }
@@ -35,16 +32,16 @@ var (
 			//manual name of the meter, to distinguish between multiple sensors
 			"meter_name",
 		})
-		gaugeHumidity = promauto.NewGaugeVec(prometheus.GaugeOpts{
-			Namespace: "dht22",
-			Name:      "humidity",
-			Help:      "Current humidity",
-		},
-			[]string{
-				//manual name of the meter, to distinguish between multiple sensors
-				"meter_name",
-			})
-		gatheringDuration = promauto.NewSummaryVec(prometheus.SummaryOpts{
+	gaugeHumidity = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dht22",
+		Name:      "humidity",
+		Help:      "Current humidity",
+	},
+		[]string{
+			//manual name of the meter, to distinguish between multiple sensors
+			"meter_name",
+		})
+	gatheringDuration = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Namespace: "dht22",
 		Name:      "gatheringduration",
 		Help:      "The duration of data gatherings",
@@ -63,7 +60,7 @@ func main() {
 
 	err = dht.HostInit()
 	if err != nil {
-		fmt.Println("HostInit error:", err)
+		log.Println("HostInit error:", err)
 		return
 	}
 
@@ -88,13 +85,13 @@ func gatherData() bool {
 
 	dht, err := dht.NewDHT(fmt.Sprintf("GPIO%d", options.Pin), dht.Celsius, "")
 	if err != nil {
-		fmt.Println("NewDHT error:", err)
+		log.Println("NewDHT error:", err)
 		return false
 	}
 
 	humidity, temperature, err := dht.ReadRetry(5)
 	if err != nil {
-		fmt.Println("Read error:", err)
+		log.Println("Read error:", err)
 		return false
 	}
 
@@ -106,6 +103,6 @@ func gatherData() bool {
 
 func logDebug(format string, v ...interface{}) {
 	if options.Debug {
-					log.Printf(format, v...)
+		log.Printf(format, v...)
 	}
 }
