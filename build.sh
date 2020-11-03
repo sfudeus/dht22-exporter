0d8d343482812f006029b792df19fb143a6ba7c1
Stephan Fudeus <sfudeus@users.noreply.github.com>
2020-11-03 21:34:36 +0100

    Mark build version dirty if it is

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [ $# -ne 1 ]; then
 fi
 
 GO_VERSION=$1
-PRG_VERSION=$(git describe --tags)
+PRG_VERSION=$(git describe --tags --dirty)
 IMAGE_VERSION=${PRG_VERSION}_${GO_VERSION}
 
 docker buildx build --build-arg "GO_VERSION=${GO_VERSION}" --platform linux/amd64 --platform linux/arm/v7 -t "sfudeus/dht22-exporter:${IMAGE_VERSION}" --push .
