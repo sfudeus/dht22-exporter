7575c355c2c1595c45746406a52da6be6c1710ec
Stephan Fudeus <sfudeus@users.noreply.github.com>
2020-11-03 22:26:06 +0100

    Add build comment

--- build.sh
+++ build.sh
@@ -11,4 +11,6 @@ GO_VERSION=$1
 PRG_VERSION=$(git describe --tags --dirty)
 IMAGE_VERSION=${PRG_VERSION}_${GO_VERSION}
 
+echo "Building version $IMAGE_VERSION"
 docker buildx build --build-arg "GO_VERSION=${GO_VERSION}" --platform linux/amd64 --platform linux/arm/v7 -t "sfudeus/dht22-exporter:${IMAGE_VERSION}" --push .
+
